442dd6b1bd18479721d59dfdd76519b90ba418cc
Alexander Ten <orionapplepie@gmail.com>
2020-02-09 21:26:49 +1000

    Убраны лишние комментарии, работа с переменными окружения убрана из функций

--- main.py
+++ main.py
@@ -35,7 +35,6 @@ def download_image(url="", img_path="", img_name="", rewrite=True):
     file_name = os.path.join(img_path, img_name)
     path = Path(file_name)
 
-    # если есть опция перезаписи и если уже есть такой файл, то не скачиваем
     if not rewrite and path.is_file():
         return
 
@@ -43,18 +42,22 @@ def download_image(url="", img_path="", img_name="", rewrite=True):
         url=url, stream=True, verify=False
     )
     response.raise_for_status()
-
-    with open(file_name, 'wb') as file:
-        for chunk in response.iter_content(DEFAULT_CHUNK_SIZE):
-            file.write(chunk)
+    try:
+        with open(file_name, 'wb') as file:
+            for chunk in response.iter_content(DEFAULT_CHUNK_SIZE):
+                file.write(chunk)
+                # raise ValueError
+    except IOError as error:
+        exit("Ошибка записи файла\n{0}\n}".format(error))
+    # finally:
+        # os.remove(file_name)
     return file_name
 
 
-def get_wall_upload_url(vk_group_id=None):
+def get_wall_upload_url(vk_group_id=None, access_token=""):
     """Получение ссылки для загрузки фото на сервер ВК
     для последующей публикации на стену группы."""
     api_method_name = "photos.getWallUploadServer"
-    access_token = os.getenv("VK_APP_ACCESS_TOKEN")
 
     params = {
         "group_id": vk_group_id,
@@ -72,7 +75,7 @@ def get_wall_upload_url(vk_group_id=None):
 
 
 def upload_image_vk(img_path="", url=""):
-    """Загрузка фото на заданный url."""
+    """Загрузка фото на заданный url. Возвращает данные о загрузке."""
     with open(img_path, "rb") as image_file:
         files = {
             "photo": image_file
@@ -82,17 +85,17 @@ def upload_image_vk(img_path="", url=""):
         return response.json()
 
 
-def save_wall_photo(upload_response=None):
-    """Сохранение фото для публикации на стене."""
+def save_wall_photo(upload_response=None, access_token=""):
+    """Сохранение загруженного фото для публикации на стене.
+    Возвращает данные о сохранении."""
     api_method_name = "photos.saveWallPhoto"
-    access_token = os.getenv("VK_APP_ACCESS_TOKEN")
 
     params = {
         "group_id": VK_MY_COMMUNITY_ID,
         "photo": upload_response["photo"],
         "server": upload_response["server"],
         "hash": upload_response["hash"],
-        "caption": "new caption!",
+
         "access_token": access_token,
         "v": VK_API_VERSION,
     }
@@ -104,10 +107,9 @@ def save_wall_photo(upload_response=None):
     return response.json()
 
 
-def post_wall_photo(save_image_response=None, message=""):
+def post_wall_photo(save_image_response=None, message="", access_token=""):
     """Публикация фото на стене группы."""
     api_method_name = "wall.post"
-    access_token = os.getenv("VK_APP_ACCESS_TOKEN")
     resp = save_image_response["response"][0]
 
     attachment_str = "".join([
@@ -145,9 +147,9 @@ def download_random_comics():
     resp = requests.get(url=url)
     resp.raise_for_status()
     
-    resp_json = resp.json()
-    comic_comment = resp_json["alt"]
-    img_url = resp_json["img"]
+    comics_data = resp.json()
+    comic_comment = comics_data["alt"]
+    img_url = comics_data["img"]
     comic_img_name = get_url_filename(img_url)
 
     img_path = download_image(
@@ -163,23 +165,34 @@ def download_random_comics():
 
 
 def main():
+    access_token = os.getenv("VK_APP_ACCESS_TOKEN")
     try:
         photo_path, message = download_random_comics().values()
 
-        upload_url = get_wall_upload_url(vk_group_id=VK_MY_COMMUNITY_ID)
-        upload_resp = upload_image_vk(photo_path, upload_url)
-
-        save_resp = save_wall_photo(upload_response=upload_resp)
-        post_resp = post_wall_photo(
-            save_image_response=save_resp, message=message
+        upload_url = get_wall_upload_url(
+            vk_group_id=VK_MY_COMMUNITY_ID,
+            access_token=access_token
         )
+        upload_response_data = upload_image_vk(
+            img_path=photo_path, url=upload_url
+        )
+
+        save_resp = save_wall_photo(
+            upload_response=upload_response_data,
+            access_token=access_token
+        )
+        post_resp = post_wall_photo(
+            save_image_response=save_resp,
+            message=message,
+            access_token=access_token
+        )
+        os.remove(photo_path)
+
     except HTTPError as error:
         exit("Невозможно получить данные с сервера:\n{0}\n".format(error))
 
     except ConnectionError as error:
         exit("Проблема с сетевым соединением:\n{0}\n".format(error))
-
-    os.remove(photo_path)
 
 
 if __name__ == "__main__":
